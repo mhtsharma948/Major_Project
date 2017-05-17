cf4ccb826e0e327cde70e986a0fc142cf23cad2c
mohit <mhtsharma948@gmail.com>
2017-05-18 01:56:49 +0530

    bug fixing

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
     <!--custom css-->
     <link rel="stylesheet" type="text/css" href="sass/stylesheets/homepage.css">
 </head>
-<body class="background" background="background.jpg">
+
 <?php
 // Start the session
 session_start();
@@ -30,19 +30,9 @@ session_start();
 <?php
     // If no session is there..
     if(empty($_SESSION["user"])) {
-
-
-        /*echo "<nav>";
-        echo "<div class='nav-wrapper  cyan lighten-2'>";
-        echo "<a href='#!' class='brand-logo'>ProjArch</a>";
-        echo "<ul class='right hide-on-med-and-down'>";
-        echo "<li><a href='signin.php'>Sign In</a></li>";
-        echo "<li><a href='signup_page.php'>Sign Up</a></li>";
-        echo "</ul>";
-        echo "</div>";
-        echo "</nav>";*/
+        echo "<body class=\"background\" background=\"background.jpg\">";
         require 'navBeforeSignin.php';
-
+        echo "<div class='background'>";
         echo "<div class=\"card col s6 transicard\" style='margin-bottom: 0px; margin-top: 0px'>";
         echo "<br><p class='flow-text center white-text text-lighten-3' style='font-size: 60px;'>PROJECT MANAGEMENT<br> MADE EASY</p>";
         echo "<p class='flow-text center  blue-grey-text text-darken-4' style='font-size: 35px;'>ProjArch makes tracking, collaborating, and reporting a no-brainer, no matter how your teams choose to work.More than 15,000 organizations manage their work in ProjArch</p>";
@@ -52,15 +42,20 @@ session_start();
         echo "<a class=\"waves-effect waves-light btn-large col s2\" href='signin.php'>Try Now</a></div>";
         echo "<br>";
         echo "</div>";
+        echo "</div>";
+        echo "<script type=\"text/javascript\" src=\"https://code.jquery.com/jquery-2.1.1.min.js\"></script>";
+        echo "<script type=\"text/javascript\" src=\"js/materialize.min.js\"></script>";
+        echo "<script>$(document).ready(function(){ $(\".button-collapse\").sideNav(); });</script>";
+        echo "</body>";
     }
     // if session exists
     else {
         echo "<br>";
         require 'main_page.php';
     }
-?>
-<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-<script type="text/javascript" src="js/materialize.min.js"></script>
-<script>$(document).ready(function(){ $(".button-collapse").sideNav(); });</script>
-</body>
+
+    ?>
+
+
+
 </html>
