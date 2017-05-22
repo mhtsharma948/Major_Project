b79a8259d1cab899bfabf159fc71a8c4726a11e1
mohit <mhtsharma948@gmail.com>
2017-05-23 00:21:54 +0530

    with love

--- navAfterSignin.php
+++ navAfterSignin.php
@@ -1,13 +1,13 @@
 <nav>
     <div class="nav-wrapper cyan lighten-2">
-        <a href="index.php" class="brand-logo">ProjArch</a>
+        <a href="../index.php" class="brand-logo">ProjArch</a>
         <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
         <ul class="right hide-on-med-and-down">
-            <li class="waves-effect waves-red"><a href='signout.php' class="waves-red">Sign Out<i class="material-icons right">power_settings_new</i></a></li>
+            <li class="waves-effect waves-red"><a href='../signout.php' class="waves-red">Sign Out<i class="material-icons right">power_settings_new</i></a></li>
         </ul>
         <ul class="side-nav" id="mobile-demo">
             <li class="cyan-text center-align"><h4>ProjArch</h4><li>
-            <li><a href='signout.php' class="waves-effect waves-red s12 l12">Sign Out<i class="material-icons right">power_settings_new</i></a></li>
+            <li><a href='../signout.php' class="waves-effect waves-red s12 l12">Sign Out<i class="material-icons right">power_settings_new</i></a></li>
         </ul>
     </div>
 </nav>
--- navBeforeSignin.php
+++ navBeforeSignin.php
@@ -1,15 +1,15 @@
 <nav>
     <div class="nav-wrapper cyan lighten-2">
-        <a href="index.php" class="brand-logo">ProjArch</a>
+        <a href="../index.php" class="brand-logo">ProjArch</a>
         <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
         <ul class="right hide-on-med-and-down">
-            <li><a href='signin.php' class="waves-effect waves-teal">Sign In<i class="material-icons right">perm_identity</i></a></li>
-            <li><a href='signupPage.php' class="waves-effect waves-teal">Sign Up<i class="material-icons right">perm_identity</i></a></li>
+            <li><a href='../signin.php' class="waves-effect waves-teal">Sign In<i class="material-icons right">perm_identity</i></a></li>
+            <li><a href='../signupPage.php' class="waves-effect waves-teal">Sign Up<i class="material-icons right">perm_identity</i></a></li>
         </ul>
         <ul class="side-nav" id="mobile-demo">
             <li class="cyan-text center-align"><h4>ProjArch</h4><li>
-            <li><a href='signin.php' class="waves-effect waves-teal">Sign In<i class="material-icons right">perm_identity</i></a></li>
-            <li><a href='signupPage.php' class="waves-effect waves-teal">Sign Up<i class="material-icons right">perm_identity</i></a></li>
+            <li><a href='../signin.php' class="waves-effect waves-teal">Sign In<i class="material-icons right">perm_identity</i></a></li>
+            <li><a href='../signupPage.php' class="waves-effect waves-teal">Sign Up<i class="material-icons right">perm_identity</i></a></li>
         </ul>
     </div>
 </nav>
